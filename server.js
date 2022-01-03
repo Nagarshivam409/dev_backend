b1ea6ccd2ed415f91f30425a5056af3f499805a5
abhi9720 <abhishektiwari9720@gmail.com>
2022-01-03 23:16:30 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require('cors')
+const connectDB = require("./config/db");
+const path = require("path");
+// connect db
+connectDB();
+// middleware
+app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders: "*",
+    credentials: true,
+  })
+);
+
+
+app.use(express.json({ extended: false })); // to get body data similar to body parser
+
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
+// Serve static assets in production
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
